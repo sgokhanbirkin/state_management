f0d97785f0190906685bdea7e9da960eb6aade27
gokhanbirkin <gkandth@gmail.com>
2022-05-10 21:34:36 +0300

    InheritedModel - Flutter State Management Course
    https://www.youtube.com/watch?v=0vl35jtpsfs&list=PL6yRaaP0WPkUf-ff1OX99DVSL1cynLHxO&index=4

---
+++ testing_inherited_model_course/android/app/src/main/kotlin/com/example/testing_inherited_model_course/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.testing_inherited_model_course
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ testing_inherited_model_course/lib/main.dart
@@ -0,0 +1,180 @@
+import 'dart:math' show Random;
+
+import 'package:flutter/material.dart';
+import 'dart:developer' as devtools show log;
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Material App',
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final title = 'Home Page';
+  var color1 = Colors.red;
+  var color2 = Colors.green;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+        centerTitle: true,
+      ),
+      body: AvailableColorsWidget(
+        color1: color1,
+        color2: color2,
+        child: Column(
+          children: [
+            Row(
+              children: [
+                TextButton(
+                  onPressed: () {
+                    setState(() {
+                      color1 = colors.getRandomElement();
+                    });
+                  },
+                  child: const Text('Change Color 1'),
+                ),
+                TextButton(
+                  onPressed: () {
+                    setState(() {
+                      color2 = colors.getRandomElement();
+                    });
+                  },
+                  child: const Text('Change Color 2'),
+                ),
+              ],
+            ),
+            const ColorWidget(color: AvailableColors.one),
+            const ColorWidget(color: AvailableColors.two),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+enum AvailableColors { one, two }
+
+class AvailableColorsWidget extends InheritedModel<AvailableColors> {
+  final MaterialColor color1;
+  final MaterialColor color2;
+
+  const AvailableColorsWidget({
+    Key? key,
+    required this.color1,
+    required this.color2,
+    required Widget child,
+  }) : super(
+          key: key,
+          child: child,
+        );
+
+  static AvailableColorsWidget of(
+    BuildContext context,
+    AvailableColors aspect,
+  ) {
+    return InheritedModel.inheritFrom<AvailableColorsWidget>(
+      context,
+      aspect: aspect,
+    )!;
+  }
+
+  @override
+  bool updateShouldNotify(covariant AvailableColorsWidget oldWidget) {
+    devtools.log('updateShouldNotify');
+    return color1 != oldWidget.color1 || color2 != oldWidget.color2;
+  }
+
+  @override
+  bool updateShouldNotifyDependent(
+    covariant AvailableColorsWidget oldWidget,
+    Set<AvailableColors> dependencies,
+  ) {
+    devtools.log('updateShouldNotifyDependent');
+
+    if (dependencies.contains(AvailableColors.one) && color1 != oldWidget.color1) {
+      return true;
+    }
+
+    if (dependencies.contains(AvailableColors.two) && color2 != oldWidget.color2) {
+      return true;
+    }
+
+    return false;
+  }
+}
+
+class ColorWidget extends StatelessWidget {
+  final AvailableColors color;
+
+  const ColorWidget({
+    Key? key,
+    required this.color,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    switch (color) {
+      case AvailableColors.one:
+        devtools.log('ColorWidget.build: one');
+        break;
+      case AvailableColors.two:
+        devtools.log('ColorWidget.build: two');
+        break;
+    }
+
+    final provider = AvailableColorsWidget.of(context, color);
+
+    return Container(
+      height: 100,
+      color: color == AvailableColors.one ? provider.color1 : provider.color2,
+    );
+  }
+}
+
+final colors = [
+  Colors.red,
+  Colors.green,
+  Colors.blue,
+  Colors.yellow,
+  Colors.orange,
+  Colors.purple,
+  Colors.pink,
+  Colors.brown,
+  Colors.grey,
+  Colors.indigo,
+  Colors.cyan,
+  Colors.lime,
+  Colors.teal,
+  Colors.lightGreen,
+  Colors.lightBlue,
+  Colors.deepPurple,
+];
+
+// examle getRandomColor()
+//final color = colors.getRandomElement();
+
+extension RandomElement<T> on Iterable<T> {
+  T getRandomElement() => elementAt(
+        Random().nextInt(length),
+      );
+}
