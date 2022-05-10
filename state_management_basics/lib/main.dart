e858cc4d683ae5a4909e1115cd7efa1b11c49fc3
gokhanbirkin <gkandth@gmail.com>
2022-05-10 16:05:38 +0300

    State Management Basics  Step - 1

---
+++ state_management_basics/lib/main.dart
@@ -0,0 +1,154 @@
+import 'package:flutter/material.dart';
+import 'package:uuid/uuid.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      home: const HomePage(),
+      debugShowCheckedModeBanner: false,
+      routes: {
+        '/new-contact': (context) => const NewContactView(),
+      },
+    );
+  }
+}
+
+class Contact {
+  final String id;
+  final String name;
+
+  Contact({
+    required this.name,
+  }) : id = const Uuid().v4();
+}
+
+class ContactBook extends ValueNotifier<List<Contact>> {
+  ContactBook._sharedInstance()
+      : super([
+          Contact(name: 'John Doe'),
+        ]);
+  static final ContactBook _shared = ContactBook._sharedInstance();
+  factory ContactBook() => _shared;
+
+  int get length => value.length;
+
+  void add({required Contact contact}) {
+    final contacts = value;
+    contacts.add(contact);
+    notifyListeners();
+  }
+
+  void remove({required Contact contact}) {
+    final contacts = value;
+    if (contacts.contains(contact)) {
+      contacts.remove(contact);
+      notifyListeners();
+    }
+  }
+
+  Contact? contact({required int atIndex}) => value.length > atIndex ? value[atIndex] : null;
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Page'),
+        centerTitle: true,
+      ),
+      body: ValueListenableBuilder(
+        valueListenable: ContactBook(),
+        builder: (context, value, child) {
+          final contacts = value as List<Contact>;
+          return ListView.builder(
+            itemCount: contacts.length,
+            itemBuilder: (context, index) {
+              final contact = contacts[index];
+              return Dismissible(
+                onDismissed: (direction) {
+                  ContactBook().remove(contact: contact);
+                },
+                key: ValueKey(contact.id),
+                child: Material(
+                  color: Colors.white,
+                  elevation: 6.0,
+                  child: ListTile(
+                    title: Text(contact.name),
+                  ),
+                ),
+              );
+            },
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          await Navigator.of(context).pushNamed('/new-contact');
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class NewContactView extends StatefulWidget {
+  const NewContactView({Key? key}) : super(key: key);
+
+  @override
+  State<NewContactView> createState() => _NewContactViewState();
+}
+
+class _NewContactViewState extends State<NewContactView> {
+  late final _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Add a new contact'),
+      ),
+      body: Column(
+        children: [
+          TextField(
+            controller: _controller,
+            decoration: const InputDecoration(
+              labelText: 'Enter the name of the new contact',
+            ),
+          ),
+          const SizedBox(height: 16),
+          TextButton(
+            onPressed: () {
+              final contact = Contact(name: _controller.text);
+              ContactBook().add(contact: contact);
+              Navigator.of(context).pop();
+            },
+            child: const Text('Add contact'),
+          ),
+        ],
+      ),
+    );
+  }
+}
